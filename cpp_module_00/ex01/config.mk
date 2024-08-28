d4c39cf0a039d21e79085326b132c234375f4f14
Lauri Litovuo <llitovuo@student.hive.fi>
2024-08-28 15:25:03 +0300

    ex01 init

---
+++ cpp_module_00/ex01/config.mk
@@ -0,0 +1,24 @@
+# Exec name
+OUTFILE := program
+
+# Source directory
+S:= srcs/
+
+# Object directory
+O := objs/
+
+# Include directory
+I := incl/
+
+# Source files
+SRC := \
+		$Smain.cpp
+# Include directories
+CXXFLAGS += -I$I
+
+# Libraries to link (add -L for library directories and -l for libraries)
+LDFLAGS :=
+
+SRC_DIR := $S
+OBJ_DIR := $O
+INC_DIR := $I
---
+++ cpp_module_00/ex01/incl/PhoneBook.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/28 14:32:13 by llitovuo          #+#    #+#             */
+/*   Updated: 2024/08/28 15:10:30 by llitovuo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+# include <iostream>
+# include <cstring>
+
+class Phonebook
+{
+
+};
+
+#endif
