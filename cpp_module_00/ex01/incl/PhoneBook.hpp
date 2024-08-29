6a5b1782230600f56042994b5ed14e46aae14cf8
Lauri Litovuo <llitovuo@student.hive.fi>
2024-08-29 13:35:58 +0300

    little advacement

--- cpp_module_00/ex01/incl/PhoneBook.hpp
+++ cpp_module_00/ex01/incl/PhoneBook.hpp
@@ -6,13 +6,14 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 14:32:13 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/08/29 12:48:44 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/08/29 12:57:15 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 # include "Contact.hpp"
+# include <iostream>
 
 class Phonebook
 {
@@ -24,7 +25,10 @@ class Phonebook
 		Contact phoneBook[MAX_COUNT];
 
 	public:
-
+		Phonebook(){
+			std::cout << "Write ADD to add, SEARCH to search or EXIT to exit";
+		}
+	
 		bool	addContact(void);
 		bool	SearchContact(void);
 		bool	DeleteContact(void);
---
+++ cpp_module_00/ex01/srcs/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Contact.hpp"
+#include "PhoneBook.hpp"
+
+int main(){
+	Phonebook Book;
+	string input;
+
+	while (1)
+	{
+		if ()
+
+	}
+	return (0);
+}
