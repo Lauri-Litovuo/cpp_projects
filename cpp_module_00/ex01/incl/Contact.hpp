8c7574860ba1672833f62b10873af0d069368e5a
Lauri Litovuo <llitovuo@student.hive.fi>
2024-08-29 12:50:13 +0300

    adding a draft for headers

--- cpp_module_00/ex01/incl/Contact.hpp
+++ cpp_module_00/ex01/incl/Contact.hpp
@@ -1,14 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/28 14:46:40 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/08/29 09:03:57 by llitovuo         ###   ########.fr       */
+/*   Created: 2024/08/29 09:44:52 by llitovuo          #+#    #+#             */
+/*   Updated: 2024/08/29 12:18:14 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PhoneBook.hpp"
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+# include <string>
 
+class Contact{
+
+	private:
+
+	public:
+		std::string	firstName;
+		std::string	lastName;
+		std::string	nickname;
+		std::string	phoneNumber;
+		std::string	darkestSecret;
+};
+
+#endif
--- cpp_module_00/ex01/incl/PhoneBook.hpp
+++ cpp_module_00/ex01/incl/PhoneBook.hpp
@@ -6,18 +6,33 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 14:32:13 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/08/28 15:10:30 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/08/29 12:48:44 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
-# include <iostream>
-# include <cstring>
+# include "Contact.hpp"
 
 class Phonebook
 {
+	#define MAX_COUNT 8
 
+	private:
+
+		int		Counter;
+		Contact phoneBook[MAX_COUNT];
+
+	public:
+
+		bool	addContact(void);
+		bool	SearchContact(void);
+		bool	DeleteContact(void);
+		void	getFirstName();
+		void	getLastName();
+		void	getNickName();
+		void	getPhoneNumber();
+		void	getSecret();
 };
 
 #endif
