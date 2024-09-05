0cea915178970538030ba3b780e21b9e98ca58a7
Lauri Litovuo <llitovuo@student.hive.fi>
2024-09-05 10:05:43 +0300

    Adding negative number check

--- cpp_module_01/ex01/srcs/main.cpp
+++ cpp_module_01/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 13:44:28 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/09/04 16:09:36 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/09/05 10:04:06 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,11 @@ int	main(int ac, char **av)
 
 	if (ac > 1)
 	{
+		if (atoi(av[1]) < 0)
+		{
+			std::cout << "Number of zombies must be positive" << std::endl;
+			return (1);
+		}
 		nbZombies = atoi(av[1]);
 		if (ac > 2)
 			zombieName = av[2];
