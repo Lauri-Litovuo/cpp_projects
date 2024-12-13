81e3851d2254ca4e8637e142300b98b5f66ac2ce
Lauri Litovuo <llitovuo@student.hive.fi>
2024-12-13 10:58:43 +0200

    adding ex01

---
+++ cpp_module_07/ex01/config.mk
@@ -0,0 +1,28 @@
+# Exec name
+OUTFILE := iter
+
+# Compiler settings
+CXX := g++
+
+# Source directory
+S:= ./srcs/
+
+# Object directory
+O := objs/
+
+# Include directory
+I := ./incl/
+
+# Source files
+SRC := \
+		$Smain.cpp
+
+# Include directories
+CXXFLAGS += -Wall -Wextra -Werror -std=c++17 -I$I
+
+# Libraries to link (add -L for library directories and -l for libraries)
+LDFLAGS :=
+
+SRC_DIR := $S
+OBJ_DIR := $O
+INC_DIR := $I
---
+++ cpp_module_07/ex01/incl/iter.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+
+template <typename T>
+void iter(T *array, size_t length, void (*func)(T const &)) {
+	for (size_t i = 0; i < length; i++) {
+		func(array[i]);
+	}
+}
+
