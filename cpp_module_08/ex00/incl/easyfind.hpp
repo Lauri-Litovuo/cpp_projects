3bf53099a7e63f9ec34ea79b70cdd3edd8536d29
Lauri Litovuo <llitovuo@student.hive.fi>
2024-12-19 13:43:06 +0200

    init module08

---
+++ cpp_module_08/ex00/incl/easyfind.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+
+
+template <typename T>
+T* easyfind(std::vector<T> haystack, int needle){
+
+};
---
+++ cpp_module_08/ex00/srcs/main.cpp
@@ -0,0 +1,20 @@
+#include "easyfind.hpp"
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include <exception>
+
+int main(){
+	std::vector<int> intVec = {};
+	std::vector<int> intVec2 = {1, 3, 55, 6, 77, 9012};
+	std::vector<float> floatVec = {1.0f, 3.5f, 55.45f, 6.1f, 77.0f, 9012.5f};
+	std::string stringu = "Hello!";
+
+	try {
+		
+	}catch (std::exception &e){
+
+	}
+}
+
