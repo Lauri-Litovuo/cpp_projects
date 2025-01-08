bc66a4ba47e827af76fe2e6ac22178260405a047
Lauri Litovuo <llitovuo@student.hive.fi>
2025-01-08 13:01:39 +0200

    draft .hpp

--- cpp_module_09/ex02/incl/PmergeMe.hpp
+++ cpp_module_09/ex02/incl/PmergeMe.hpp
@@ -5,3 +5,22 @@
 #include <list>
 #include <chrono>
 
+struct pair{
+	int min;
+	int max;
+};
+
+class PMergeMe {
+	public:
+		PMergeMe(std::string);
+		~PMergeMe();
+		PMergeMe(const PMergeMe &src);
+		PMergeMe &operator=(const PMergeMe &rhs);
+
+		bool validateInput();
+		void sortList();
+		void sortDeque();
+		void printResults();
+	private:
+		std::deque ()
+};
