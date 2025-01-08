6dfe71765c447b81844479a6f3345d4d158d4de8
Lauri Litovuo <llitovuo@student.hive.fi>
2025-01-08 11:07:29 +0200

    drafting pmergeme

--- cpp_module_09/ex02/srcs/main.cpp
+++ cpp_module_09/ex02/srcs/main.cpp
@@ -1,3 +1,33 @@
 #include <iostream>
+#include <string>
+#include "PmergeMe.hpp"
 
-int main(int ac, )
+int main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		std::cerr << "Usage: ./PmergeMe <sequence of positive integers>" << std::endl;
+		return 1;
+	}
+	std::string sequence;
+	for (int i = 1; i < ac; i++)
+	{
+		std::string arg(av[i]);
+		if (arg.find_first_not_of("0123456789") != std::string::npos)
+		{
+			std::cerr << "Error: invalid input" << std::endl;
+			return 1;
+		}
+		sequence += arg;
+		if (i + 1 < ac)
+			sequence += " ";
+	}
+	try {
+		std::cout << sequence << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
